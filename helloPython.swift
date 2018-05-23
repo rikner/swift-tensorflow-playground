01ed3fd9b403d26fa3ddcd80054460efd9ed0ad6
rikner <rikner@web.de>
2018-05-23 23:06:49 +0200

    init

---
+++ helloPython.swift
@@ -0,0 +1,5 @@
+    import Python
+
+    let numpy = Python.import("numpy")
+    let scipy = Python.import("scipy")
+    let sklearn = Python.import("sklearn")
---
+++ helloTensor.swift
@@ -0,0 +1,11 @@
+#!swift -O
+
+import TensorFlow
+
+var x = Tensor<Float>([[1, 2], [3, 4]])
+
+for i in 1...5 {
+  x += x ⊗ x
+}
+
+print(x)
