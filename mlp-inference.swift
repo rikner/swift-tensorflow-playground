eb5c99005921bf8388b566f2d676870bd112b3de
rikner <rikner@web.de>
2018-05-24 00:07:22 +0200

    restructure mlp-inference

--- mlp-inference.swift
+++ mlp-inference.swift
@@ -7,29 +7,32 @@ struct MLPClassifier {
     var w2 = Tensor<Float>(shape: [4, 1], scalars: [0.4, -0.5, -0.5, 0.4])
     var b1 = Tensor<Float>([0.2, -0.3, -0.3, 0.2])
     var b2 = Tensor<Float>([[0.4]])
+            
+    let activationFunction = hyperbolicTangentFunction
 
     func prediction(for x: Tensor<Float>) -> Tensor<Float> {
         // The ⊗ operator performs matrix multiplication.
-
         let weightedSum1 = (x ⊗ w1 + b1)
-        let o1 = hyperbolicTangentActivation(weightedSum: weightedSum1)
+        let o1 = activationFunction(weightedSum1)
         
         let weightedSum2 = (o1 ⊗ w2 + b2)
-        let o2 = hyperbolicTangentActivation(weightedSum: weightedSum2)
+        let o2 = activationFunction(weightedSum2)
         
         return o2
     }
-    
-    // logistic function as activation function
-    private func logisticActivation(weightedSum: Tensor<Float>) -> Tensor<Float> {
-        return pow((1 + exp(-weightedSum)), Float(-1))
-    }
-    
-    // hyperbolic tangent as activation function
-    private func hyperbolicTangentActivation(weightedSum: Tensor<Float>) -> Tensor<Float> {
-        return tanh(weightedSum)
-    }
 }
+
+    
+// logistic function as activation function
+private func logisticFunction(_ input: Tensor<Float>) -> Tensor<Float> {
+    return pow((1 + exp(-input)), Float(-1))
+}
+
+// hyperbolic tangent as activation function
+private func hyperbolicTangentFunction(_ input: Tensor<Float>) -> Tensor<Float> {
+    return tanh(input)
+}
+
 let input = Tensor<Float>([[0.2, 0.8]])
 let classifier = MLPClassifier()
 let prediction = classifier.prediction(for: input)
